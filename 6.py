6bb9c8033a0fc2b31449ceda9c80cdfabe96fd02
brady8756 <95857599+brady8756@users.noreply.github.com>
2022-01-07 11:07:44 +0800

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-import random
-
 # st.beta container():
 st.columns(1)
 # dict = {}
@@ -20,8 +18,7 @@ def check():
     or dict[3] == dict[5]== dict[7] != ' ' ):
         return True
 
-# 列印方法
-def drawBoard(board):
+def print_board():
     st.write(dict[7], '|' , dict[8] , '|', dict[9])
     st.write('- + - + -') 
     st.write(dict[4], '|' , dict[5] , '|', dict[6])
@@ -29,28 +26,29 @@ def drawBoard(board):
     st.write(dict[1], '|' , dict[2] , '|', dict[3])
      
 def main( ):
-    move1 = int(text_input('你想走哪一格?'))
+    move1 = int(text_input('你想怎麼走?'))
     if dict[move1] == ' ':
       dict[move1] = 'X'
     else:
-        st.write('這是錯誤的移動')
-    print_board
+        print('這是被禁止的')
+    print_board()
 
-    move2 = int(text_input('你想走哪一格?'))
+    move2 = int(text_input('你想怎麼走?'))
     if dict[move2] == ' ':
         dict[move2] = 'O'
     else:
-        st.write('這是錯誤的移動')
-    print_board
+        print('這是被禁止的')
+    print_board()
     
     if check():
-        st.write('game over')
+        print('game over')
     else:
         main()
 
-a = st.text_input('想玩井字遊戲嗎？（是/否): ')
+
+a = st.test_input('想玩井字遊戲嗎？（是/否): ')
 if a =='是':
 
-    main( )
+    main()
 else:
-    wt.write('請你離開!')
+    wt.write('可憐')
